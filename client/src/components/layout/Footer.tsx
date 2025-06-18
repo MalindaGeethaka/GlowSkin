946a7db98dcca11c3ca7178d914d70c2fb921d9b
MalindaGeethaka <malindamanesha@gmail.com>
2025-06-18 19:06:18 +0530

    update the footer year

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -92,7 +92,7 @@ const Footer: React.FC = () => {
         </div>
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 GlowSkin. All rights reserved. Made with ❤️ for beautiful skin.</p>
+          <p>&copy; 2025 GlowSkin. All rights reserved. Made with ❤️ for beautiful skin.</p>
         </div>
       </div>
     </footer>
